1f1e4292c0cfccf3da188756529f9ed3f4d49f53
Ahmet KÜPELİKILINÇ <ahmetkupelikilinc@gmail.com>
2022-04-06 14:26:28 +0300

    Klaösrler oluşturuldu.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Flatlist, Text, Image } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+      <Text>cc</Text>
+      </View>
+    </SafeAreaView>
+);
+}
+
+export default App;
